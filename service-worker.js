c7d17463c364390915f84189aa22ac6f4b4afd69
jrios93 <jcb.rios93@gmail.com>
2024-10-18 08:51:18 -0500

    intento de aplicación pwa

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "abecedario-biblico-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles/index.css",
+  "./scripts/script.js",
+  "./Assets/icon512_maskable.png",
+  "./Assets/icon512_rounded.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
